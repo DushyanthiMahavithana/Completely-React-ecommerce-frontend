17f046dc6015d6b9097aaf1c83d276a6d47483f6
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-01-09 17:06:17 +0530

    shop page update

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -6,15 +6,23 @@ import '../Styles/Shop.css'
 
 function Shop() {
   return (
-    <div className='shopBanner'>
-        <div className='header' style={{backgroundImage: `url(${ShopBanner})`}}>
-            <h1>Best Designer Handbags to <br></br>Invest Now</h1>
-            <p>Find the latest styles of Women's Handbags and more <br></br> from top fashion designers today!</p>
-            <Link to="/recipes" >
-                <button>LATEST DESIGNS</button>
-            </Link>
+    <div>
+        <div className='shopBanner'>
+            <div className='header' style={{backgroundImage: `url(${ShopBanner})`}}>
+                <h1>Best Designer Handbags to <br></br>Invest Now</h1>
+                <p>Find the latest styles of Women's Handbags and more <br></br> from top fashion designers today!</p>
+                <Link to="/recipes">
+                    <button>LATEST DESIGNS</button>
+                </Link>
+            </div>
+        </div>
+        <div className='womenBags'>
+            <h1 className='second_header'><i>"Introducing our newest line of handbags,<br></br> now available at your favorite retailers</i></h1>
+            
+        
         </div>
     </div>
+    
   )
 }
 
