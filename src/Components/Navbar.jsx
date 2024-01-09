1d9e6aa70fd10df44f979372d48c896a1c90c450
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-01-09 11:03:06 +0530

    completed creating the navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
+import Logo from '../Images/logo.jpg'
+import ReorderIcon from '@material-ui/icons/Reorder';
+import "../Styles/Navbar.css"
 
 
-function Navbar() {
+
+function Navbar() { 
+  const [openLinks, setOpenLinks] = useState(false);
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  }
   return (
-    <div className='menu'>
+    <div className='navbar'>
+      <div className='logo' id={openLinks ? "open" : "close"}>
+        <img src={Logo}></img>
+        <div className='hidden-menu-icon'>
+          <Link to='/'>Shop</Link>
+          <Link to='/handbags'>Hand Bags</Link>
+          <Link to='/backpacks'>Back Packs </Link>
+        </div>
+      </div>
+      
+      <div className='menu'>
         <Link to='/'>Shop</Link>
         <Link to='/handbags'>Hand Bags</Link>
         <Link to='/backpacks'>Back Packs </Link>
+      </div>
+
+      <div className='menu-icon'>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon></ReorderIcon>
+        </button>
+      </div>
     </div>
   )
 }
