209a0269c70158ed14832a52d9114e3021f89d08
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-01-10 12:40:19 +0530

    shop page second section

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import ShopBanner from '../Images/shopbanner.jpg'
+import WomenImage1 from '../Images/women1.jpg'
+import WomenImage2 from '../Images/women2.jpg'
+import WomenImage3 from '../Images/women3.jpg'
+import WomenImage4 from '../Images/women4.jpg'
+import WomenImage5 from '../Images/women5.jpg'
 import '../Styles/Shop.css'
 
 
@@ -18,7 +23,13 @@ function Shop() {
         </div>
         <div className='womenBags'>
             <h1 className='second_header'><i>"Introducing our newest line of handbags,<br></br> now available at your favorite retailers</i></h1>
-            
+            <div className='secondImagesSection'>
+                <div className='womenBagsImages'><img src={WomenImage1}></img></div>
+                <div className='womenBagsImages'><img src={WomenImage4}></img></div>
+                <div className='womenBagsImages'><img src={WomenImage2}></img></div>
+                <div className='womenBagsImages'><img src={WomenImage3}></img></div>
+                <div className='womenBagsImages'><img src={WomenImage5}></img></div>
+            </div>
         
         </div>
     </div>
