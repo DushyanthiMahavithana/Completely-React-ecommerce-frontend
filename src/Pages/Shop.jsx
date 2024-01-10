df68c16df002d15280e5f963101b7272af5b2601
Dushyanthi Mahavithana <115853505+DushyanthiMahavithana@users.noreply.github.com>
2024-01-10 13:23:50 +0530

    shop page third section

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -6,6 +6,11 @@ import WomenImage2 from '../Images/women2.jpg'
 import WomenImage3 from '../Images/women3.jpg'
 import WomenImage4 from '../Images/women4.jpg'
 import WomenImage5 from '../Images/women5.jpg'
+import MenImage1 from '../Images/men1.jpg'
+import MenImage2 from '../Images/men2.jpg'
+import MenImage3 from '../Images/men3.jpg'
+import MenImage4 from '../Images/men4.jpg'
+import MenImage5 from '../Images/men5.jpg'
 import '../Styles/Shop.css'
 
 
@@ -24,13 +29,32 @@ function Shop() {
         <div className='womenBags'>
             <h1 className='second_header'><i>"Introducing our newest line of handbags,<br></br> now available at your favorite retailers</i></h1>
             <div className='secondImagesSection'>
-                <div className='womenBagsImages'><img src={WomenImage1}></img></div>
+            <   div className='womenBagsImages'><img src={WomenImage1}></img></div>
                 <div className='womenBagsImages'><img src={WomenImage4}></img></div>
                 <div className='womenBagsImages'><img src={WomenImage2}></img></div>
                 <div className='womenBagsImages'><img src={WomenImage3}></img></div>
                 <div className='womenBagsImages'><img src={WomenImage5}></img></div>
             </div>
+            <div className='latestCollection'>
+                <h2>Latest Design Ladies Handbags</h2>
+                
+            </div>
         
+        </div>
+        <div className='backpacks'>
+            <h1 className='second_header'><i>"Extremely comfortable to carry all day long,<br></br> Making your days easier and extra productive</i></h1>
+            <div className='secondImagesSection'>
+                <div className='backpacksImages'><img src={MenImage4}></img></div>
+                <div className='backpacksImages'><img src={MenImage1}></img></div>
+                <div className='backpacksImages'><img src={MenImage2}></img></div>
+                <div className='backpacksImages'><img src={MenImage3}></img></div>
+                <div className='backpacksImages'><img src={MenImage5}></img></div>
+            </div>
+            <div className='latestCollection'>
+                <h2>Latest Design Backpacks</h2>
+                
+            </div>
+
         </div>
     </div>
     
